542c661e9d895226b1642c261a27cdf2fcb50238
Amin Hotari <amin.hotari89@gmail.com>
2022-09-12 19:37:05 +0300

    for-loop-basic1

---
+++ Python/Fundamentals/for_loop_basic1/for_loop_basic1.py
@@ -0,0 +1,39 @@
+for i in range(0,151,1):
+    print(i)
+
+x = 5
+while x < 1001:
+    print(x)
+    x = x + 5
+
+for a in range(1,101,1):
+    if a % 10 == 0:
+        print("Coding Dojo")
+    elif a % 5 == 0:
+        print("Coding")
+    else:
+        print(a)   
+
+sum = 0
+for odd in range (0,500001,1):
+    if odd % 2 == 1:
+        sum += odd    
+print(sum)
+
+
+z = 2018
+while z > 0:
+    print(z)
+    z = z - 4    
+
+
+lowNum = 1
+highNum = 15
+mult = 4
+
+for num in range (1,16,1):
+        if num % mult == 0:
+                print(num)
+
+       
+        

ddbd7be2a30a901c623605ab4fd3c43dd5a43861
Amin Hotari <amin.hotari89@gmail.com>
2022-09-12 14:06:55 +0300

    python-hello-world

---
+++ Python/Fundamentals/hello_world.py
@@ -0,0 +1,21 @@
+
+print("Hello World")
+name = "Amin!"
+print("Hello",name)
+print("Hello"+name)
+num = 30
+print("Hello",num)
+num = 30
+print(f"Hello + {num}")
+
+food = "Salad"
+food2 = "Beef"
+print("I love to eat {} and {}" .format(food, food2))
+print(f"I love to eat {food} and {food2}")
+
+my = "Hello %d" % 30
+print(my)
+
+me = "Amin"
+print(me.upper())
+

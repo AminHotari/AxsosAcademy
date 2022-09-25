1892b410925ab80b038404da902b2961d6c3093e
Amin Hotari <amin.hotari89@gmail.com>
2022-09-25 23:39:28 +0300

    Modify_Users_Shell

--- Python/Django/Django_ORM/Users_Shell/single_model_orm/users_app/models.py
+++ Python/Django/Django_ORM/Users_Shell/single_model_orm/users_app/models.py
@@ -2,7 +2,7 @@ from unittest.util import _MAX_LENGTH
 from venv import create
 from django.db import models
 
-class Users(models.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     email = models.CharField(max_length=45)

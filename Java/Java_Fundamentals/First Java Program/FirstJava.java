d82162f5c026dc21e5bfcd5132a763ceda4c18b6
Amin Hotari <amin.hotari89@gmail.com>
2022-10-24 11:28:19 +0300

    Add_First_Java_Program

---
+++ Java/Java_Fundamentals/First Java Program/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Amin Hotari");
+        System.out.println("I am 150 years old");
+        System.out.println("My hometown is Qalqilya, QL");
+    }
+}
